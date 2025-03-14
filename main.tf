182a0808b397098524a5611ec2306524e2000bfc
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-14 19:33:25 +0530

    changes

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 module "vpc" {
   source = "git::https://github.com/Revanthsatyam/tf-module-vpc-d76.git"
 
-  env = var.env
+  env  = var.env
+  tags = var.tags
 
   for_each = var.vpc
   cidr     = each.value["cidr"]
