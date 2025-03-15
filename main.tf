f5ea3901d335067e65e6761ec984b27b1ff91da9
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-15 14:52:31 +0530

    changes

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 module "vpc" {
   source = "git::https://github.com/Revanthsatyam/tf-module-vpc-d76.git"
 
-  env                    = var.env
-  tags                   = var.tags
-  default_vpc_id         = var.default_vpc_id
+  env                        = var.env
+  tags                       = var.tags
+  default_vpc_id             = var.default_vpc_id
   default_vpc_cidr           = var.default_vpc_cidr
   default_vpc_route_table_id = var.default_vpc_route_table_id
 
