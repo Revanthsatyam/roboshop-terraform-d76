874bbd86a1644320cba3d19523185158d4c0dbab
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-17 18:49:03 +0530

    changes

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
-output "vpc" {
-  value = [ for k,v in lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnet", null): v.id ]
-}
+# output "vpc" {
+#   value = module.vpc
+# }
