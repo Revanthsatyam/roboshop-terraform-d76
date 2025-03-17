4132f120be7a1f113a6de4e6dedcd7548c0cc96a
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-17 18:27:41 +0530

    changes

--- main.tf
+++ main.tf
@@ -12,16 +12,17 @@ module "vpc" {
   subnets  = each.value["subnets"]
 }
 
-# module "alb" {
-#   source = "git::https://github.com/Revanthsatyam/tf-module-alb-d76.git"
-#
-#   env            = var.env
-#   tags           = var.tags
-#
-#   for_each           = var.alb
-#   internal           = each.value["internal"]
-#   load_balancer_type = each.value["load_balancer_type"]
-#   sg_port            = each.value["port"]
-#   ssh_ingress        = each.value["ssh_ingress"]
-#   vpc_id             = each.value["internal"] ? local.vpc_id : var.default_vpc_id
-# }
+module "alb" {
+  source = "git::https://github.com/Revanthsatyam/tf-module-alb-d76.git"
+
+  env            = var.env
+  tags           = var.tags
+  subnets        = lookup(data.aws_subnets.default_vpc_subnets, "ids", null)
+
+  for_each           = var.alb
+  internal           = each.value["internal"]
+  load_balancer_type = each.value["load_balancer_type"]
+  sg_port            = each.value["port"]
+  ssh_ingress        = each.value["ssh_ingress"]
+  vpc_id             = each.value["internal"] ? local.vpc_id : var.default_vpc_id
+}
