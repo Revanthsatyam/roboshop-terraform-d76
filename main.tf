dbf6f798f183e0441b743e4c8f5244e64532f1bc
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-18 18:09:25 +0530

    changes

--- main.tf
+++ main.tf
@@ -41,4 +41,5 @@ module "docdb" {
   backup_retention_period = each.value["backup_retention_period"]
   preferred_backup_window = each.value["preferred_backup_window"]
   skip_final_snapshot     = each.value["skip_final_snapshot"]
+  instance_class          = each.value["instance_class"]
 }
