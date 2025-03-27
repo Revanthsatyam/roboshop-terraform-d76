9e652cca14bdbb8104c6e32f9220315039531d30
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-27 13:18:15 +0530

    changes

--- main.tf
+++ main.tf
@@ -126,7 +126,6 @@ module "app" {
   vpc_id             = local.vpc_id
   sg_ingress_cidr    = local.app_subnets_cidr
   subnet_id          = local.app_subnets
-  #availability_zones = local.app_subnets
 
   for_each         = var.app
   app_name         = each.key
