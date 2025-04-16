7766c6b14f854a88d70fe745be8d9b0774c356b4
Revanthsatyam <satyamrevanth@gmail.com>
2025-04-16 11:31:19 +0530

    changes

--- locals.tf
+++ locals.tf
@@ -4,8 +4,8 @@ locals {
   db_subnets       = [ for k,v in lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "db", null), "subnet", null): v.id ]
   app_subnets_cidr = [for k, v in lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnet", null) : v.cidr_block]
 
-  private_alb_dns      = lookup(lookup(lookup(module.alb, "private", null), "alb", null), "dns_name", null)
-  public_alb_dns       = lookup(lookup(lookup(module.alb, "public", null), "alb", null), "dns_name", null)
-  private_alb_listener = lookup(lookup(lookup(module.alb, "private", null), "listener", null), "arn", null)
-  public_alb_listener  = lookup(lookup(lookup(module.alb, "public", null), "listener", null), "arn", null)
+#   private_alb_dns      = lookup(lookup(lookup(module.alb, "private", null), "alb", null), "dns_name", null)
+#   public_alb_dns       = lookup(lookup(lookup(module.alb, "public", null), "alb", null), "dns_name", null)
+#   private_alb_listener = lookup(lookup(lookup(module.alb, "private", null), "listener", null), "arn", null)
+#   public_alb_listener  = lookup(lookup(lookup(module.alb, "public", null), "listener", null), "arn", null)
 }
