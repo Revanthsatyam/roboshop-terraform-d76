c89d5df3e9927987d32466fe02bbf9d92d75a479
Revanthsatyam <satyamrevanth@gmail.com>
2025-04-20 18:29:58 +0530

    changes

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 PARAMETERS="catalogue.prod.DOCUMENTDB,DOCUMENTDB"
 for i in $PARAMETERS; do
-  aws_ssm_pm_1=$i | awk -F "," '{print $1}'
-  echo $aws_ssm_pm_1
+  echo $i | awk -F "," '{print $1}'
 done
