5c50f03394e9f345913c2c0bfb7553a5e8fc5605
Revanthsatyam <satyamrevanth@gmail.com>
2025-04-20 18:45:35 +0530

    changes

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ PARAMETERS="catalogue.prod.DOCUMENTDB,DOCUMENTDB"
 for i in $PARAMETERS; do
   aws_ssm_pm_1=$(echo "$i" | awk -F , '{print $1}')
   aws_ssm_pm_2=$(echo "$i" | awk -F , '{print $2}')
-  echo "$aws_ssm_pm_1 $aws_ssm_pm_2"
+  export $aws_ssm_pm_2=$(aws ssm get-parameter --name "${aws_ssm_pm_1}" --with-decryption --query "Parameter.Value" --output text)
 done
