df9aebd7aee6c19d28a2e33319bfc5db9a657b2d
Revanthsatyam <satyamrevanth@gmail.com>
2025-04-20 19:02:18 +0530

    changes

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ for i in $PARAMETERS; do
   aws_ssm_pm_1=$(echo "$i" | awk -F , '{print $1}')
   aws_ssm_pm_2=$(echo "$i" | awk -F , '{print $2}')
   aws_parameter=$(aws ssm get-parameter --name "${aws_ssm_pm_1}" --with-decryption --query "Parameter.Value" --output text)
-  export abc=123
+  export abcd=123
 done
