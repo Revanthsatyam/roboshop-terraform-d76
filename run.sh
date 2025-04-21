207807cc7857cf010a7a7ecc2a3b446d73f92a21
Revanthsatyam <satyamrevanth@gmail.com>
2025-04-21 17:26:49 +0530

    changes

--- run.sh
+++ run.sh
@@ -4,3 +4,13 @@
 #  aws_ssm_pm_2=$(echo "$i" | awk -F , '{print $2}')
 #  export "${aws_ssm_pm_2}"=$(aws ssm get-parameter --name "${aws_ssm_pm_1}" --with-decryption --query "Parameter.Value" --output text)
 #done
+
+#schema_type=DOCDB
+#
+#if [ "${schema_type}" == "DOCDB" ]; then
+#
+#fi
+
+aws_ssm() {
+  echo aws ssm get-parameter --name "$1" --with-decryption --query "Parameter.Value"
+}
