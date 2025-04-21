9a0e00cf9aa68a39132551261994be9281aa5d64
Revanthsatyam <satyamrevanth@gmail.com>
2025-04-21 17:20:54 +0530

    changes

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-PARAMETERS="catalogue.prod.DOCUMENTDB,DOCUMENTDB"
-for i in $PARAMETERS; do
-  aws_ssm_pm_1=$(echo "$i" | awk -F , '{print $1}')
-  aws_ssm_pm_2=$(echo "$i" | awk -F , '{print $2}')
-  export "${aws_ssm_pm_2}"=$(aws ssm get-parameter --name "${aws_ssm_pm_1}" --with-decryption --query "Parameter.Value" --output text)
-done
+#PARAMETERS="catalogue.prod.DOCUMENTDB,DOCUMENTDB"
+#for i in $PARAMETERS; do
+#  aws_ssm_pm_1=$(echo "$i" | awk -F , '{print $1}')
+#  aws_ssm_pm_2=$(echo "$i" | awk -F , '{print $2}')
+#  export "${aws_ssm_pm_2}"=$(aws ssm get-parameter --name "${aws_ssm_pm_1}" --with-decryption --query "Parameter.Value" --output text)
+#done
